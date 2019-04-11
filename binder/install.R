6cc8e183ac4dfebf520bde823dd1d48d2210bac3
Ed Jee <edjee96@gmail.com>
2019-04-11 19:04:55 +0100

    initial binder additions

---
+++ binder/install.R
@@ -0,0 +1,25 @@
+## Packages we need to install
+
+packages <- c("RFast",
+              "margins",
+              "dplyr",
+              "ggplot2",
+              "ggExtra",
+              "grf",
+              "tibble",
+              "AER",
+              "stargazer",
+              "purrr",
+              "sandwich",
+              "scales",
+              "tidyr",
+              "gt",
+              "haven",
+              "broom",
+              "grf",
+              "modelr",
+              "furrr")
+install.packages(packages)
+
+
+
